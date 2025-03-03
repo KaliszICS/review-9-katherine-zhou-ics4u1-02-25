d74cad166baf8bd1dcca8886b46277672aaa489b
katherine-zhou-ics4u1-02-25 <katherine.zhou3@student.tdsb.on.ca>
2025-03-03 03:09:03 +0000

    Update Practcie

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,23 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-
+ 
 	}
 
+	public static int lettersToFive(String sent, char letter){
+		int num = 0;
+		int times = 0;
+		while (num<sent.length()){
+			if (sent.charAt(num)== letter){
+				times++;
+			}
+			
+			if (times == 5){
+				return num;
+			}
+			num++;
+		}
+		return -1;
+
+}
 }
